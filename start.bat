eda0f8a40563eb2d46c6d706d1bcfb79a083e44b
Leeseongrok <mission_los@icloud.com>
2019-08-23 22:17:31 +0900

    Paper Update
    Paper 빌드로 전환
    자체 제작 스크립트에 정상적으로 불러올경우 콘솔 출력

--- start.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-echo [UnderWorld] 20190810 started
-java -Xmx2048M -jar spigot-1.13.2.jar -o true
---
+++ 시작.bat
@@ -0,0 +1,5 @@
+@echo off
+chcp 65001
+color f0
+type logo.txt
+java -Xmx2048M -jar paper-642.jar -o true
